667dec63ad9ee88ed65687f79250ae7c6f99bbab
Johannes Ruthmann <j.ruthmann@online.de>
2026-06-17 22:03:33 +0200

    fix

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -4,12 +4,14 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "*",
+]
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_NAME'),
+        'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST'),
